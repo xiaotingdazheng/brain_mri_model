ec27e6682c36805f55cd93c66c9b789d1a67df1d
BBillot <benjamin.billot1993@gmail.com>
2018-11-13 15:49:49 +0000

    register synthetic images to real test image

--- labelFusion.m
+++ labelFusion.m
@@ -40,45 +40,50 @@ for i=1:size(leaveOneOutIndices,1)
     labelMap = zeros([size(realImage.vol), length(labelsList)]); %initialise matrix
     
     %compute binary mask of ROI within real image
-    flo = cellPathsRealImages{leftOutIndex(j)}; %path of real image
-    [dir,name,ext] = fileparts(flo);
+    ref = cellPathsRealImages{leftOutIndex(i)}; %path of real image
+    [dir,name,ext] = fileparts(ref);
     stripped = fullfile(dir,[name,'.stripped',ext]); %path of stripped real image
     fmask = fullfile(dir,[name,'.mask',ext]); %path of binary mask
-    cmd = ['~/Software/ROBEX/runROBEX.sh ' flo ' ' stripped ' ' fmask]; 
+    cmd = ['~/Software/ROBEX/runROBEX.sh ' ref ' ' stripped ' ' fmask]; 
     [~,~] = system(cmd); %compute fmask
     
     for j=1:size(leaveOneOutIndices,2)
         
-        ref = cellPathsSyntheticImages{leaveOneOutIndices(i,j)}; %path of synthetic image
-        [dir,name,ext] = fileparts(ref);
+        floSynthetic = cellPathsSyntheticImages{leaveOneOutIndices(i,j)}; %path of synthetic image
+        [dir,name,ext] = fileparts(floSynthetic);
         stripped = fullfile(dir,[name,'.stripped',ext]); %path of stripped synthetic image
         rmask = fullfile(dir,[name,'.mask',ext]); %path of binary mask
-        res = fullfile(dir,[name,'.registered_to_image',num2str(i),ext]); %path of registered real image
-        aff = ''; %modify name of the saved aff file
+        resSynthetic = fullfile(dir,[name,'.registered_to_image_',num2str(leftOutIndex(i)),ext]); %path of registered real image
+        trans = fullfile(dir,[name,'.registered_to_image_',num2str(leftOutIndex(i)),'.cpp']); %modify name of the saved aff file
         
-        if ~exist(res, 'file')
-            %compute binary mask of ROI synthetic image
-            cmd = ['~/Software/ROBEX/runROBEX.sh ' ref ' ' stripped ' ' rmask];
-            [~,~] = system(cmd);
-        end
+        % compute binary mask of ROI synthetic image
+        cmd = ['~/Software/ROBEX/runROBEX.sh ' floSynthetic ' ' stripped ' ' rmask];
+        [~,~] = system(cmd);
         
-        %compute registration
-        cmd = ['reg_aladin -ref ',ref,' -flo ',flo',' -res ',res,' -rmask ',rmask,' -fmask ',fmask,' -aff ',aff,'-rigOnly -nac -pad 0 -voff'];
-        system(cmd)
-       
-        registeredRealImage = MRIread(res);
-        syntheticImage = MRIread(ref);
-        segmentationMap = MRIread(cellPathsLabels{leaveOneOutIndices(i,j)});
-       
-        if registeredRealImage.volsize ~= syntheticImage.volsize || registeredRealImage.volsize ~= segmentationMap.volsize
+        % compute registration synthetic image to real image
+        cmd = ['reg_f3d -ref ',ref,' -flo ',floSynthetic',' -res ',resSynthetic,' -rmask ',rmask,' -fmask ',fmask,' -cpp ',trans,' -pad 0 -voff'];
+        [~,~] = system(cmd);
+        
+        % apply registration to segmentation map
+        floSegm = cellPathsLabels{leaveOneOutIndices(i,j)}; % path of segmentation map to apply register
+        resSegm = ''; % path of registered segmentation map
+        cmd = ['reg_resample ',ref,' -flo ',floSegm,' -trans ',trans,' -res ',resSegm,' -pad 0 -voff'];
+        [~,~] = system(cmd);
+        
+        % read registered real,synthetic and segmentation images
+        realImage = MRIread(ref);
+        registeredSyntheticImage = MRIread(resSynthetic);
+        registeredSegmentationMap = MRIread(resSegm);
+        
+        if realImage.volsize ~= registeredSyntheticImage.volsize || realImage.volsize ~= registeredSegmentationMap.volsize
             error('registered image doesn t have the same size as synthetic image')
         end
-       
+        
         % calculate similarity between test (real) image and training (synthetic) image
-        likelihood = 1/sqrt(2*pi*sigma)*exp(-(registeredRealImage.vol-syntheticImage.vol)^2/(2*sigma^2));
+        likelihood = 1/sqrt(2*pi*sigma)*exp(-(realImage.vol-registeredSyntheticImage.vol)^2/(2*sigma^2));
         
         for k=1:length(labelsList)
-            labelMap(:,:,:,k) = labelMap(:,:,:,k) + (segmentationMap.vol == labelsList(k)).*likelihood;
+            labelMap(:,:,:,k) = labelMap(:,:,:,k) + (registeredSegmentationMap.vol == labelsList(k)).*likelihood;
         end
        
     end
