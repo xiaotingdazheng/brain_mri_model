975807c549d322b390d49a491e836cdcf9260b0c
BBillot <benjamin.billot1993@gmail.com>
2018-11-30 18:38:45 +0000

    changed logOdds to be saved as nii.gz files

--- labels2prob.m
+++ labels2prob.m
@@ -6,22 +6,25 @@ addpath /home/benjamin/matlab/toolbox
 % create sufolder if it doesn't exist
 if ~exist(pathLogOddsSubfolder, 'dir'), mkdir(pathLogOddsSubfolder), end
 
-Labels = MRIread(pathLabels);
-Labels = Labels.vol;
+LabelsMRI = MRIread(pathLabels);
+Labels = LabelsMRI.vol;
+LabelsMask = Labels > 1;
 
 % loop over all the labels
 for l=1:length(labelsList)-1
-%     
-%     mask = (Labels == labelsList(l)); % find mask of current label
-%     erudedMask = imerode(mask,ones(2,2,2)); % erode mask
-%     prob = exp(-rho*bwdist(erudedMask)); % calculate prob of voxel belonging to label l
-%     thresholdMap = prob > threshold; 
-%     prob = prob.*thresholdMap; % threshold prob map
-%     
-%     % save label probability in separate file
-%     temp_path = fullfile(pathLogOddsSubfolder, ['logOdds_' num2str(labelsList(l)) '.mat']);
-%     save(temp_path, 'prob');
-%     
+    
+    mask = (Labels == labelsList(l)); % find mask of current label
+    erudedMask = imerode(mask,ones(2,2,2)); % erode mask
+    prob = exp(-rho*bwdist(erudedMask)); % calculate prob of voxel belonging to label l
+    thresholdMap = prob > threshold; 
+    prob = prob.*thresholdMap; % threshold prob map
+    prob = prob.*LabelsMask;
+    LabelsMRI.vol = prob; % write new prob map in mgz file
+    
+    % save label probability in separate file
+    temp_path = fullfile(pathLogOddsSubfolder, ['logOdds_' num2str(labelsList(l)) '.nii.gz']);
+    MRIwrite(LabelsMRI, temp_path);
+    
 end
 
 % calculate logOdds for whole hippocampus
@@ -30,9 +33,11 @@ erudedMask = imerode(mask,ones(2,2,2)); % erode mask
 prob = exp(-rho*bwdist(erudedMask)); % calculate prob of voxel belonging to label l
 thresholdMap = prob > threshold;
 prob = prob.*thresholdMap; % threshold prob map
+prob = prob.*LabelsMask;
+LabelsMRI.vol = prob;
 
 % save label probability in separate file
-temp_path = fullfile(pathLogOddsSubfolder, ['logOdds_' num2str(labelsList(l+1)) '.mat']);
-save(temp_path, 'prob');
+temp_path = fullfile(pathLogOddsSubfolder, ['logOdds_' num2str(labelsList(l+1)) '.nii.gz']);
+MRIwrite(LabelsMRI, temp_path);
 
 end
