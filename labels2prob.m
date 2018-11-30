20b959538f785352b7aee7e22858535829f0bea2
BBillot <benjamin.billot1993@gmail.com>
2018-11-30 16:56:31 +0000

    fix bugs concerning hippocampus logOdds path

--- labels2prob.m
+++ labels2prob.m
@@ -11,17 +11,17 @@ Labels = Labels.vol;
 
 % loop over all the labels
 for l=1:length(labelsList)-1
-    
-    mask = (Labels == labelsList(l)); % find mask of current label
-    erudedMask = imerode(mask,ones(2,2,2)); % erode mask
-    prob = exp(-rho*bwdist(erudedMask)); % calculate prob of voxel belonging to label l
-    thresholdMap = prob > threshold; 
-    prob = prob.*thresholdMap; % threshold prob map
-    
-    % save label probability in separate file
-    temp_path = fullfile(pathLogOddsSubfolder, ['logOdds_' num2str(labelsList(l)) '.mat']);
-    save(temp_path, 'prob');
-    
+%     
+%     mask = (Labels == labelsList(l)); % find mask of current label
+%     erudedMask = imerode(mask,ones(2,2,2)); % erode mask
+%     prob = exp(-rho*bwdist(erudedMask)); % calculate prob of voxel belonging to label l
+%     thresholdMap = prob > threshold; 
+%     prob = prob.*thresholdMap; % threshold prob map
+%     
+%     % save label probability in separate file
+%     temp_path = fullfile(pathLogOddsSubfolder, ['logOdds_' num2str(labelsList(l)) '.mat']);
+%     save(temp_path, 'prob');
+%     
 end
 
 % calculate logOdds for whole hippocampus
@@ -32,7 +32,7 @@ thresholdMap = prob > threshold;
 prob = prob.*thresholdMap; % threshold prob map
 
 % save label probability in separate file
-temp_path = fullfile(pathLogOddsSubfolder, ['logOdds_' num2str(labelsList(l)) '.mat']);
+temp_path = fullfile(pathLogOddsSubfolder, ['logOdds_' num2str(labelsList(l+1)) '.mat']);
 save(temp_path, 'prob');
 
 end
