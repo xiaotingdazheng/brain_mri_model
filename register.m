565f92c540e6067df9276d44ab04e73f819bdf55
BBillot <benjamin.billot1993@gmail.com>
2019-01-10 18:23:37 +0000

    modified filenames in register.m

--- register.m
+++ register.m
@@ -1,22 +1,17 @@
 function [pathRegisteredFloatingImage, pathRegisteredFloatingLabels, pathRegisteredFloatingHippoLabels, pathTransformation] = register(pathRefMaskedImage, ...
-    pathFloatingImage, pathFloatingLabels, pathFloatingHippoLabels, labelPriorType, resultsFolder, refIndex, recompute, floBrainNum)
+    pathFloatingImage, pathFloatingLabels, pathFloatingHippoLabels, labelPriorType, resultsFolder, recompute, refBrainNum)
 
 % names of files that will be used/saved during registration
 temp_pathSyntheticImage = strrep(pathFloatingImage,'.nii.gz','.mgz');
 [~,filename,~] = fileparts(temp_pathSyntheticImage);
-if ~contains(filename, 'brain') && contains(pathFloatingImage, 'brain')
-    brain_num = floBrainNum;
-    filename = [brain_num '_' filename];
-end
-pathRegisteredFloatingImage = fullfile(resultsFolder,[filename '.registered_to_image_' num2str(refIndex) '.nii.gz']); %path of registered real image
+pathRegisteredFloatingImage = fullfile(resultsFolder,[filename '.registered_to_' refBrainNum '.nii.gz']); %path of registered floating image
 aff = fullfile(resultsFolder, [filename,'.aff']); %deformation of first registration
-pathAffineTransformation = fullfile(resultsFolder, [filename '.registered_to_image_' num2str(refIndex) '.affine.nii.gz']); %path of registered real image
-pathTransformation = fullfile(resultsFolder, [filename '.registered_to_image_' num2str(refIndex) '.cpp.nii.gz']); %modify name of the saved aff file
+pathTransformation = fullfile(resultsFolder, [filename '.registered_to_' refBrainNum '.cpp.nii.gz']); %modify name of the saved aff file
 
 % compute first rigid registration
 if ~exist(aff, 'file') || recompute
     disp(['registering with reg_aladin ',pathFloatingImage,' to ',pathRefMaskedImage]);
-    cmd = ['reg_aladin -ref ' pathRefMaskedImage ' -flo ' pathFloatingImage ' -aff ' aff ' -res ' pathAffineTransformation ' -pad 0 -voff'];
+    cmd = ['reg_aladin -ref ' pathRefMaskedImage ' -flo ' pathFloatingImage ' -aff ' aff ' -pad 0 -voff'];
     system(cmd);
 end
 % compute registration synthetic image to real images
@@ -31,7 +26,7 @@ if isequal(labelPriorType, 'delta function')
     % define pathnames of used/saved files for label registration
     temp_floSegm = strrep(pathFloatingLabels,'.nii.gz','.mgz');
     [~,filename,~] = fileparts(temp_floSegm);
-    pathRegisteredFloatingLabels = fullfile(resultsFolder, [filename,'.registered_to_image_',num2str(refIndex),'.nii.gz']); % path of registered segmentation map
+    pathRegisteredFloatingLabels = fullfile(resultsFolder, [filename,'.registered_to_',refBrainNum,'.nii.gz']); % path of registered segmentation map
     % apply registration to segmentation map
     if ~exist(pathRegisteredFloatingLabels, 'file') || recompute
         disp(['applying ',pathTransformation,' to ',pathFloatingLabels]);
@@ -42,7 +37,7 @@ if isequal(labelPriorType, 'delta function')
     % same mechanism for hippocampus segmentation map
     temp_floSegm = strrep(pathFloatingHippoLabels,'.nii.gz','.mgz');
     [~,filename,~] = fileparts(temp_floSegm);
-    pathRegisteredFloatingHippoLabels = fullfile(resultsFolder, [filename,'.registered_to_image_',num2str(refIndex),'.nii.gz']); % path of registered segmentation map
+    pathRegisteredFloatingHippoLabels = fullfile(resultsFolder, [filename,'.registered_to_',refBrainNum,'.nii.gz']); % path of registered segmentation map
     % apply registration to segmentation map
     if ~exist(pathRegisteredFloatingHippoLabels, 'file') || recompute
         disp(['applying ',pathTransformation,' to ',pathFloatingHippoLabels]);
