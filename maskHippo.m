2426c764d4ba3885847f4ad1f99633f48ec1c1b2
BBillot <benjamin.billot1993@gmail.com>
2019-01-10 17:59:37 +0000

    changed name of saved hippo labels for delta function

--- maskHippo.m
+++ maskHippo.m
@@ -1,14 +1,10 @@
-function pathFloatingHippoLabels = maskHippo(pathFloatingLabels, resultsFolder, floBrainNum, recompute)
+function pathFloatingHippoLabels = maskHippo(pathFloatingLabels, resultsFolder, recompute)
 
 % builds name of hippo label file that will be saved
 temp_lab = strrep(pathFloatingLabels,'.nii.gz','');
 [~,name,~] = fileparts(temp_lab);
-if ~contains(name, 'brain')
-    brain_num = [floBrainNum '_'];
-else
-    brain_num = '';
-end
-pathFloatingHippoLabels = fullfile(resultsFolder, [brain_num name '.hippo_segmentation_map.nii.gz']);
+strrep(name, 'labels', 'hippo_labels');
+pathFloatingHippoLabels = fullfile(resultsFolder, [name, '.nii.gz']);
 
 % if file doesn't exist or must be recomputed
 if ~exist(pathFloatingHippoLabels, 'file') || recompute
@@ -16,9 +12,7 @@ if ~exist(pathFloatingHippoLabels, 'file') || recompute
     setFreeSurfer();
     
     FloatingLabels = MRIread(pathFloatingLabels); % read labels
-    
     hippoMap = FloatingLabels.vol > 20000 | FloatingLabels.vol == 17 | FloatingLabels.vol == 53; % hippo mask
-    
     FloatingLabels.vol = hippoMap; 
     
     MRIwrite(FloatingLabels, pathFloatingHippoLabels); % write new file
