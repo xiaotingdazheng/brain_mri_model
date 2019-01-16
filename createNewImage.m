6220eeb9a7ee87cf6c3a38a6de01d6c0f4e5688e
BBillot <benjamin.billot1993@gmail.com>
2019-01-16 14:54:27 +0000

    new version of createNewImage

--- createNewImage.m
+++ createNewImage.m
@@ -1,5 +1,4 @@
-function new_image = createNewImage(preprocessedLabelsMRI, classesStats, listClassesToGenerate, labelsList, labelClasses, gaussianType, targetRes,...
-    pathNewImagesFolder, pathImageResliceLike, brainNum, imageModality, smoothingName)
+function createNewImage(pathTrainingLabels, classesStats, targetResolution, pathTempImageSubfolder)
 
 % This script generates a synthetic image from a segmentation map and basic
 % statistics of intensity distribution for all the regions in the brain.
@@ -9,61 +8,62 @@ function new_image = createNewImage(preprocessedLabelsMRI, classesStats, listCla
 % We still need to blur the obtained image before downsample it to the
 % desired target resolution, before saving the final result.
 
-preprocessedLabels = preprocessedLabelsMRI.vol;
-sampleRes=[preprocessedLabelsMRI.xsize, preprocessedLabelsMRI.ysize, preprocessedLabelsMRI.zsize];
+% internal usage of labels and associated classes
+labelsList = [  2,3, 4, 5,7,8,10,11,12,13,14,15,16,17,18,24,26,28,30,31,41,42,43,44,46,47,49,50,51,52,...
+    53,54,58,60,62,63,85,251,252,253,254,255,20001,20002,20004,20005,20006,20101,20102,20104,20105,20106];
+labelClasses = [2,1,12,12,4,3, 7, 8,10,11,12,12, 5,14,15,16, 9, 6,18,13, 2, 1,12,12, 4, 3, 7, 8,10,11,...
+    14,15, 9, 6,18,13,17,  2,  2,  2,  2,  2,   14,   14,   14,   14,    2,   14,   14,   14,   14,    2];
 
-%classesStats: matrix containing stats computed for each class
-% 1st row = mean
-% 2nd row = median
-% 3rd row = standard deviation
-% 4th row = 1.4826*median absolute deviation (ie sigmaMAD)
+% read training labels
+labelsMRI = MRIread(pathTrainingLabels);
+labels = labelsMRI.vol;
 
-new_image = zeros(size(preprocessedLabels));
-
-for lC=1:length(listClassesToGenerate)
+% create new image by sampling from intensity prob distribution
+new_image = zeros(size(labels));
+for lC=1:length(unique(labelClasses))
     disp(['generating voxel intensities for class ', num2str(lC)]);
     
+    % find labels belonging to class lC
     classLabel = listClassesToGenerate(lC);
-    labelsBelongingToClass = labelsList(labelClasses == classLabel); %labels belonging to class lC
+    labelsBelongingToClass = labelsList(labelClasses == classLabel); 
+    
+    % sample from prob distribution lC
     for l=1:length(labelsBelongingToClass)
-        voxelIndices = find(preprocessedLabels == labelsBelongingToClass(l)); %find voxels with label l
-        if strcmp(gaussianType, 'median')
-            new_image(voxelIndices) = classesStats(2,classLabel) + classesStats(4,classLabel)*sqrt(8)*randn(size(voxelIndices)); %generate new values for these voxels
-        elseif strcmp(gaussianType, 'mean')
-            new_image(voxelIndices) = classesStats(1,classLabel) + classesStats(1,classLabel)*sqrt(8)*randn(size(voxelIndices)); %generate new values for these voxels
-        else
-            error('gaussianType not recognised. Should be "mean" or "median"')
-        end
+        voxelIndices = find(labels == labelsBelongingToClass(l)); %find voxels with label l
+        new_image(voxelIndices) = classesStats(2,classLabel) + classesStats(4,classLabel)*sqrt(8)*randn(size(voxelIndices)); %generate new values for these voxels
     end
+    
 end
 
 % blurring images
 disp('blurring image to prevent alliasing');
-f=targetRes./sampleRes;
+sampleRes = [labelsMRI.xsize, labelsMRI.ysize, labelsMRI.zsize]; % should be 0.3
+f=targetResolution./sampleRes;
 sigmaFilt=0.9*f;
 new_image = imgaussfilt3(new_image, sigmaFilt); %apply gaussian filter
 
-disp('writting created image');
-preprocessedLabelsMRI.vol = new_image;
-
 % names of created files (image and segmentation)
-if targetRes(1) == targetRes(2) && targetRes(1) == targetRes(3)
-    resolution = num2str(targetRes(1),'%.1f');
+TrainingBrainNum = pathTrainingLabels(regexp(pathTrainingLabels,'brain'):regexp(pathTrainingLabels,'_labels')-1);
+if targetResolution(1) == targetResolution(2) && targetResolution(1) == targetResolution(3)
+    resolution = num2str(targetResolution(1),'%.1f');
 else
-    resolution = [num2str(targetRes(1),'%.1f'), 'x',num2str(targetRes(2),'%.1f'), 'x',num2str(targetRes(3),'%.1f')];
+    resolution = [num2str(targetResolution(1),'%.1f'), 'x',num2str(targetResolution(2),'%.1f'), 'x',num2str(targetResolution(3),'%.1f')];
 end
-pathNewImage = fullfile(pathNewImagesFolder, [brainNum,'.',imageModality,'.synthetic.',resolution,'.',smoothingName,'nii.gz']);
-pathNewSegmMap = fullfile(pathNewImagesFolder, [brainNum,'.',imageModality,'.synthetic.',resolution,'.',smoothingName,'labels.nii.gz']);
+pathNewImage = fullfile(pathTempImageSubfolder, 'synthetic_images', ['training_' TrainingBrainNum '.synthetic.' resolution '.nii.gz']);
+pathNewSegmMap = fullfile(pathTempImageSubfolder, 'synthetic_labels', ['training_' TrainingBrainNum '_labels.synthetic.' resolution '.nii.gz']);
+voxsize = [num2str(targetResolution(1),'%.1f') ' ' num2str(targetResolution(2),'%.1f') ' ' num2str(targetResolution(3),'%.1f')];
 
 % save temporary image (at sampling resolution)
-MRIwrite(preprocessedLabelsMRI, pathNewImage); %write a new nifti file.
+disp('writting created image');
+labelsMRI.vol = new_image;
+MRIwrite(labelsMRI, pathNewImage); %write a new nifti file.
 
 % save image and labels at target resolution
 disp('dowmsampling to target resolution');
 setFreeSurfer();
-cmd = ['mri_convert ' pathNewImage ' ' pathNewImage ' -rl ' pathImageResliceLike ' -rt nearest -odt float']; % downsample like template image
+cmd = ['mri_convert ' pathNewImage ' ' pathNewImage ' -voxsize ' voxsize ' -rt cubic -odt float']; % downsample like template image
 [~,~] = system(cmd);
-cmd = ['mri_convert ' preprocessedLabelsMRI.fspec ' ' pathNewSegmMap ' -rl ' pathImageResliceLike ' -rt nearest -odt float']; % same for labels
+cmd = ['mri_convert ' labelsMRI.fspec ' ' pathNewSegmMap ' -voxsize ' voxsize ' -rt nearest -odt float']; % same for labels
 [~,~] = system(cmd);
 
 end
