f8c5ff44258fd2bc9ca7f4440b0e259d0d52d17a
BBillot <benjamin.billot1993@gmail.com>
2019-02-22 09:51:14 +0000

    added masking of ref image in image synthesis

--- synthetiseTrainingImages.m
+++ synthetiseTrainingImages.m
@@ -1,4 +1,4 @@
-function [pathDirSyntheticImages, pathDirSyntheticLabels, pathRefImage] = synthetiseTrainingImages(pathRefImage, pathFirstLabels, pathDirTrainingLabels,...
+function [pathDirSyntheticImages, pathDirSyntheticLabels, pathRefImage] = synthetiseTrainingImages(pathRefImage, pathRefFirstLabels, pathDirTrainingLabels,...
     pathClassesTable, targetRes, recompute, freeSurferHome, niftyRegHome, debug, rescale)
 
 % files handling
@@ -6,6 +6,7 @@ structPathsTrainingLabels = dir(pathDirTrainingLabels);
 idx = regexp(pathRefImage,'brain');
 refBrainNum = pathRefImage(idx(end):regexp(pathRefImage,'.nii.gz')-1);
 pathTempImageSubfolder = fullfile(fileparts(structPathsTrainingLabels(1).folder), ['temp_' refBrainNum]);
+preprocessedRefBrainFolder = fullfile(pathTempImageSubfolder, 'test_images_preprocessed');
 if ~exist(pathTempImageSubfolder, 'dir'), mkdir(pathTempImageSubfolder); end
 pathStatsMatrix = fullfile(pathTempImageSubfolder, 'ClassesStats.mat');
 
@@ -13,15 +14,18 @@ if rescale
     pathRefImage = rescaleIntensities(pathRefImage, refBrainNum, recompute);
 end
 
+% mask ref image with first labels
+pathRefImage = maskImage(pathRefImage, pathRefFirstLabels, preprocessedRefBrainFolder, freeSurferHome);
+
 % compute stats from reference image
-classesStats = computeIntensityStats(pathRefImage, pathFirstLabels, pathClassesTable, pathStatsMatrix, recompute);
+classesStats = computeIntensityStats(pathRefImage, pathRefFirstLabels, pathClassesTable, pathStatsMatrix, recompute);
 
 % create images from stats using training labels
 for i=1:length(structPathsTrainingLabels)
     
     pathTrainingLabels = fullfile(structPathsTrainingLabels(i).folder, structPathsTrainingLabels(i).name);
     [pathDirSyntheticImages, pathDirSyntheticLabels] = createNewImage(pathTrainingLabels, classesStats, targetRes, ...
-        pathTempImageSubfolder, pathRefImage, pathFirstLabels, recompute, freeSurferHome, niftyRegHome, debug);
+        pathTempImageSubfolder, pathRefImage, pathRefFirstLabels, recompute, freeSurferHome, niftyRegHome, debug);
 
 end
 
