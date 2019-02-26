1395ec857e9302763ee926a54f67fac1935868b9
BBillot <benjamin.billot1993@gmail.com>
2019-02-26 16:45:25 +0000

    removed masking from synthetising function

--- synthetiseTrainingImages.m
+++ synthetiseTrainingImages.m
@@ -6,16 +6,12 @@ structPathsTrainingLabels = dir(pathDirTrainingLabels);
 idx = regexp(pathRefImage,'brain');
 refBrainNum = pathRefImage(idx(end):regexp(pathRefImage,'.nii.gz')-1);
 pathTempImageSubfolder = fullfile(fileparts(fileparts(pathRefImage)), ['temp_' refBrainNum]);
-preprocessedRefBrainFolder = fullfile(pathTempImageSubfolder, 'test_images_preprocessed');
 if ~exist(pathTempImageSubfolder, 'dir'), mkdir(pathTempImageSubfolder); end
 pathStatsMatrix = fullfile(pathTempImageSubfolder, 'ClassesStats.mat');
 
 if rescale
     pathRefImage = rescaleIntensities(pathRefImage, refBrainNum, recompute);
 end
-
-% mask ref image with first labels
-pathRefImage = maskImage(pathRefImage, pathRefFirstLabels, preprocessedRefBrainFolder, freeSurferHome);
 
 % compute stats from reference image
 classesStats = computeIntensityStats(pathRefImage, pathRefFirstLabels, pathClassesTable, pathStatsMatrix, recompute);
