017aa57b6bd12c78d73eb25bf2e8a81591bb979f
BBillot <benjamin.billot1993@gmail.com>
2019-03-04 18:35:08 +0000

    updated reading and checking params

--- readParams.m
+++ readParams.m
@@ -1,5 +1,5 @@
 function [leaveOneOut, useSynthethicImages, recompute, debug, deleteSubfolder, targetResolution, rescale, margin, rho, threshold, sigma, ...
-    labelPriorType, registrationOptions, freeSurferHome, niftyRegHome] = readParams(params)
+    labelPriorType, regOptions, freeSurferHome, niftyRegHome] = readParams(params, nChannel)
 
 %read params
 leaveOneOut = params{1};
@@ -14,7 +14,7 @@ rho = params{9};
 threshold = params{10};
 sigma = params{11};
 labelPriorType = params{12};
-registrationOptions = params{13};
+regOptions = params{13};
 freeSurferHome = params{14};
 niftyRegHome = params{15};
 
@@ -31,6 +31,8 @@ if rho<=0, error('rho should be a strictly positive number'); end
 if threshold<0, error('threshold should be a positive or null number'); end
 if sigma<=0, error('sigma should be a strictly positive number'); end
 if ~isequal(labelPriorType,'logOdds') && ~isequal(labelPriorType,'delta function'), error('labelPriorType should be "delta_function" or "logOdds"'); end
+if length(regOptions)==1, regOptions=repmat(regOptions,1,nChannel); 
+elseif length(regOptions)~=nChannel, error('registrationOptions should be of length 1 (same for all channels) or of same length as channel number'); end
 setFreeSurfer(freeSurferHome); [a,~]=system('mri_convert --help'); if a, error('freeSurferHome not recognised'); end
 [a,~]=system([niftyRegHome '/reg_f3d --help']); if a, error('niftyRegHome not recognised'); end
 
