2e8da20888e0dd517419b853ad31fb9b463baaa5
BBillot <benjamin.billot1993@gmail.com>
2019-04-10 18:25:34 +0100

    completed info of cropped nifty file in cropLabelVol

--- toolbox/cropLabelVol.m
+++ toolbox/cropLabelVol.m
@@ -11,8 +11,10 @@ V=mri.vol;
 
 if label
     if isequal(label,'hippo')
+        % crop around hiipo
         idx = find(V>20000 | V == 17 | V == 53);
     else
+        % crop around a set of labels
         idx=[];
         for i=1:length(label)
             temp_idx=find(label(i));
@@ -20,6 +22,7 @@ if label
         end
     end
 else
+    % crop around all positive values
     idx = find(V>0.01);
 end
 
@@ -41,10 +44,17 @@ maxK=min(size(V,3),maxK);
 
 cropping=[minI maxI minJ maxJ minK maxK];
 
+% build new nitfy file
 mri2=[];
 mri2.vol=V(minI:maxI,minJ:maxJ,minK:maxK);
 v2r=mri.vox2ras0;
 v2r(1:3,4)=v2r(1:3,4)+v2r(1:3,1:3)*[minJ-1; minI-1; minK-1];
 mri2.vox2ras0=v2r;
+mri2.vox2ras1=v2r;
+mri2.vox2ras=v2r;
+mri2.xsize = mri.xsize; mri2.ysize = mri.ysize; mri2.zsize = mri.zsize;
+mri2.volres = [mri.xsize mri.ysize mri.zsize];
+mri2.tr = mri.tr; mri2.te = mri.te; mri2.ti = mri.ti;
+mri2.volsize = size(mri2.vol);
 
 end
